e2f734e35b1f3e309804586811f3521137fce7b5
andiserg <sergienkoandrew9922@gmail.com>
2024-12-06 00:48:42 +0200

    create lab#1

---
+++ lab1/src/webapp/controllers/info.py
@@ -0,0 +1,25 @@
+﻿from importlib import resources
+
+from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
+
+router = APIRouter()
+
+@router.get("/privacy")
+async def privacy():
+    file_path = resources.files("webapp.views") / 'privacy.html'
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    return HTMLResponse(content=content)
+
+
+@router.get("/about", response_class=HTMLResponse)
+async def get_about_page():
+    file_path = resources.files("webapp.views") / 'about.html'
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    return HTMLResponse(content=content)
